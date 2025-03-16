f2768eab2c79d192e0d342feecb714000ff9374b
Eril Smith <balletswimmer@gmail.com>
2025-03-16 13:47:48 -0700

    Update ocd_dashboard_queries.sql

--- ocd_dashboard_queries.sql
+++ ocd_dashboard_queries.sql
@@ -1,74 +1,72 @@
--- 1. Count & Pct of F vs M that hacce OCD & Average Obsession score by Gender
-with gender_count_data as (
-SELECT
-Gender, 
-COUNT(`Patient ID`) as patient_count,
-round(avg(`Y-BOCS Score (Obsessions)`),2) as avg_obs_score
+-- OCD Patient Dashboard SQL Queries
+-- This file contains SQL queries used to extract and transform data for the OCD Patient Dashboard.
+-- Source: OCD Patient Dataset from Kaggle
+-- Author: Eril Smith
 
+-- 1. Count & Percent of Female vs Male OCD Patients & Average Obsession score by Gender
+
+WITH gender_count_data AS 
+(
+SELECT Gender
+  ,COUNT(`Patient ID`) AS patient_count
+  ,round(avg(`Y-BOCS Score (Obsessions)`),2) AS avg_obs_score
  FROM OCD_Patient_Dataset.ocd_health_data
- Group by Gender
- Order by patient_count
+   GROUP BY Gender
+   ORDER BY patient_count
  )
  
-SELECT 
- sum(case when Gender = 'Female' then patient_count else 0 end) as count_female,
- sum(case when Gender = 'Male' then patient_count else 0 end) as count_male,
-
- round(sum(case when Gender = 'Female' then patient_count else 0 end)/
- (sum(case when Gender = 'Female' then patient_count else 0 end)+sum(case when Gender = 'Male' then patient_count else 0 end)) *100,2)
- as pct_female,
-
-round(sum(case when Gender = 'Male' then patient_count else 0 end)/
- (sum(case when Gender = 'Female' then patient_count else 0 end)+sum(case when Gender = 'Male' then patient_count else 0 end)) *100,2)
- as pct_male
-
- FROM gender_count_data
+SELECT sum(CASE WHEN Gender = 'Female' THEN patient_count ELSE 0 END) AS count_female
+  ,sum(CASE WHEN Gender = 'Male' THEN patient_count ELSE 0 END) AS count_male
+  ,round(sum(CASE WHEN Gender = 'Female' THEN patient_count ELSE 0 END)/
+ (sum(CASE WHEN Gender = 'Female' THEN patient_count ELSE 0 END)+sum(CASE WHEN Gender = 'Male' THEN patient_count ELSE 0 END)) *100,2)
+ AS pct_female
+  ,round(sum(CASE WHEN Gender = 'Male' THEN patient_count ELSE 0 END)/
+ (sum(CASE WHEN Gender = 'Female' THEN patient_count ELSE 0 END)+sum(CASE WHEN Gender = 'Male' THEN patient_count ELSE 0 END)) *100,2)
+ AS pct_male
+FROM gender_count_data
  ;
 
--- 2. Count of Patients by Ethnicity and their respective Average Obsession Score
+-- 2. Count of Patients by Ethnicity and Respective Average Obsession Score
 
-SELECT
-Ethnicity, 
-count(`Patient ID`) as patient_count,
-round(avg(`Y-BOCS Score (Obsessions)`),2) as avg_obs_score
+SELECT Ethnicity
+  ,COUNT(`Patient ID`) AS patient_count
+  ,round(avg(`Y-BOCS Score (Obsessions)`),2) AS avg_obs_score
 FROM OCD_Patient_Dataset.ocd_health_data
-Group by Ethnicity
-Order by patient_count
+  GROUP BY Ethnicity
+  ORDER BY patient_count
 ;
 
 
 3. Number of people diagnosed with OCD MoM (month over month)
 
-alter table OCD_Patient_Dataset.ocd_health_data
-modify `OCD Diagnosis Date` date;
+ALTER TABLE OCD_Patient_Dataset.ocd_health_data
+MODIFY `OCD Diagnosis Date` date
+;
 
-select
-date_format(`OCD Diagnosis Date`, "%Y-%m-01 00:00:00") as month,
-count(`Patient ID`) as patient_count
+SELECT date_format(`OCD Diagnosis Date`, "%Y-%m-01 00:00:00") AS MONTH
+  ,COUNT(`Patient ID`) AS patient_count
 FROM OCD_Patient_Dataset.ocd_health_data 
-group by month
-order by month 
+  GROUP BY MONTH
+  ORDER BY MONTH 
 ;
 
 -- 4. What is the most common Obsession Type (Count) & it's respective Average Obsession Score
 
-select
-`Obsession Type`,
-count(`Patient ID`) as patient_count,
-round(avg(`Y-BOCS Score (Obsessions)`),2) as avg_obs_score
-from OCD_Patient_Dataset.ocd_health_data 
-group by `Obsession Type`
-order by patient_count
+SELECT `Obsession Type`
+  ,COUNT(`Patient ID`) AS patient_count
+  ,ROUND(AVG(`Y-BOCS Score (Obsessions)`),2) AS avg_obs_score
+FROM OCD_Patient_Dataset.ocd_health_data 
+  GROUP BY `Obsession Type`
+  ORDER BY patient_count
 ;
 
 
 -- 5. What is the most common Compulsion type (Count) & it's respective Average Obsession Score
 
-select
-`Compulsion Type`,
-count(`Patient ID`) as patient_count,
-round(avg(`Y-BOCS Score (Compulsions)`),2) as avg_com_score
-from OCD_Patient_Dataset.ocd_health_data 
-group by `Compulsion Type`
-order by patient_count
+SELECT `Compulsion Type`
+  ,COUNT(`Patient ID`) AS patient_count
+  ,ROUND(AVG(`Y-BOCS Score (Compulsions)`),2) AS avg_com_score
+FROM OCD_Patient_Dataset.ocd_health_data 
+  GROUP BY `Compulsion Type`
+  ORDER BY patient_count
 ;
